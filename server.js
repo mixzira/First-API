c3819d679b6d644c97997219c5dd3ecc22acba5a
mixzira <mixzira@outlook.com.br>
2022-06-14 16:32:39 -0300

    Created and defined the routing system

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const http = require('http') // Requires standard HTTP protocol module.
 const port = '3000' // Setting default port used by local server.
+const routs = { // Creating routes to our API.
+    '/': 'First API',
+    '/books': 'I entered the book page',
+    '/authors': 'I entered the authors page',
+    '/categories': 'I entered the categories page'
+}
 
 const server = http.createServer((req, res) => { // Creating server and defining request and response parameters.
     res.writeHead(200, {'Content-Type': 'text/plain'}) // Defining response type.
-    res.end('First API') // Finalizing response and delivering content.     
+    res.end(routs[req.url]) // Finalizing response and delivering content.     
 })
 
 server.listen(port, () => { // Listening to server and generating console log.
