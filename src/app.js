52a85bd7829a28f07b59a085764f444254c36d10
mixzira <mixzira@outlook.com.br>
2022-06-14 17:13:18 -0300

    Beginning of CRUD creation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express' // Importing express module.
 
 const app = express()
+
+app.use(express.json())
+
 const books = [ // Creating an Array with Books 
     {
         id: 1,
@@ -18,6 +21,11 @@ app.get('/', (req, res) => { // Using express with no defined route.
 
 app.get('/books', (req, res) => { // Creating a defined route for response.
     res.status(200).json(books) // Returning a Json array.
+})
+
+app.post('/books', (req, res) => { // Indicates data to be added from a request.
+    books.push(req.body) // Adds request content to books variable.
+    res.status(201).send('The book has been successfully registered.') // Replies operation success. 
 })
 
 export default app // Creating an export for the content of this script.
